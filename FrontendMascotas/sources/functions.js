a17a01b7c31ce1e117bb6c63e550867bd884b746
ariza1992 <ariza1992@hotmail.com>
2022-11-16 17:55:26 -0500

    Formulario Usuarios

---
+++ FrontendMascotas/sources/functions.js
@@ -0,0 +1,60 @@
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }else{
+                    RegistrarUsuario();
+                    event.preventDefault()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+//Funcion para capturar el envio del formulario
+
+function RegistrarUsuario(){
+    let cedula = document.querySelector("#txtCedula").value;
+    let nombre = document.querySelector("#txtNombres").value;
+    let apellido = document.querySelector("#txtApellidos").value;
+    let telefono = document.querySelector("#txtTelefono").value;
+    let correo = document.querySelector("#txtCorreo").value;
+    let rol = document.querySelector("#txtRol").value;
+
+    let url = `http://localhost:3000/usuarios`;
+    let datos = {
+        cedula: cedula,
+        nombre: nombre,
+        apellido: apellido,
+        telefono: telefono,
+        correo: correo,
+        rol: rol
+    };
+
+    fetch(url,{
+        method: "POST",
+        body: JSON.stringify(datos),
+        headers:{
+            "Content-Type": "application/json"
+        }
+    }).then(res => res.json())
+    .then(mensaje => {
+        console.log(mensaje)
+    })
+
+    //alert("Usuario registrado exitosamente, revisa tu correo para las credenciales de acceso")
+}
+
